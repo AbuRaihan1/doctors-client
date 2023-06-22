f587e28d1e9cd6f9194ea73202b1b225fe6b7160
Abu Raihan <raihanahmed01973@gmail.com>
2023-06-22 00:32:10 -0500

    react query initialized

--- src/Pages/Appointment/AppointmentItems/AppointmentItems.js
+++ src/Pages/Appointment/AppointmentItems/AppointmentItems.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 import AppointmentModal from "../AppointmentModal/AppointmentModal";
 import AppointmentItem from "./AppointmentItem";
+import { useQuery } from "@tanstack/react-query";
 
 const AppointmentItems = ({ selectedDay }) => {
-  const [appointmentItems, setAppointmentItems] = useState([]);
+  // const [appointmentItems, setAppointmentItems] = useState([]);
   const [treatMent, setTreatMent] = useState({});
-  useEffect(() => {
-    fetch("doctorsService.json")
-      .then((res) => res.json())
-      .then((data) => setAppointmentItems(data));
-  }, []);
+
+  const { data: appointmentItems = [] } = useQuery({
+    queryKey: ["doctorsService"],
+    queryFn: () =>
+      fetch("http://localhost:5000/doctorsService").then((res) => res.json()),
+  });
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/doctorsService")
+  //     .then((res) => res.json())
+  //     .then((data) => setAppointmentItems(data));
+  // }, []);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {appointmentItems.map((appointmentItem, idx) => (
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import AuthProvider from "./Context/AuthContext";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
